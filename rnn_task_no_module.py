f02c21742b7f9fa150f3f88421fe4407c8cbdac6
Max Terrae Dickens <47918604+MTDickens@users.noreply.github.com>
2024-07-03 18:05:51 +0800

    Update

--- rnn_task_no_module.py
+++ rnn_task_no_module.py
@@ -9,7 +9,7 @@ import torch.optim as optim
 import numpy as np
 import matplotlib.pyplot as plt
 
-DATA_SIZE = 500
+DATA_SIZE = 1000
 
 sine_data_size = np.random.randint(int(0.3 * DATA_SIZE), int(0.7 * DATA_SIZE))
 sigmoid_data_size = DATA_SIZE - sine_data_size
@@ -37,24 +37,91 @@ data = torch.Tensor(data)
 from torch.utils.data import random_split
 train_set, test_set = random_split(data, [0.8, 0.2])
 
+# define network
 class SimpleClassificationRNN(nn.Module):
     def __init__(self, hidden_size):
         super(SimpleClassificationRNN, self).__init__()
-        # self.rnn = nn.RNN(input_size=1,
-        #                   hidden_size=hidden_size,
-        #                   batch_first=True,
-        #                   num_layers=4,
-        #                   bidirectional=True)
-        self.rnn = nn.LSTM(input_size=1,
-                            hidden_size=hidden_size,
-                            batch_first=True,
-                            num_layers=4)
+        input_size = 1
+        self.weight_ih = torch.zeros((input_size, hidden_size), requires_grad=True)
+        self.weight_hh = torch.zeros((hidden_size, hidden_size), requires_grad=True)
+        self.b_ih = torch.zeros(hidden_size, requires_grad=True)
+        self.b_hh = torch.zeros(hidden_size, requires_grad=True)
+        self.linear = nn.Linear(hidden_size, 1, bias=True)
+
+    def forward(self, seq, hc=None):
+        for i in range(seq.shape[1]):
+            if hc is None:
+                hc = torch.zeros_like(self.b_hh)
+            hc = torch.tanh(torch.matmul(seq[:, i], self.weight_ih) + self.b_ih + torch.matmul(hc, self.weight_hh) + self.b_hh)
+        x = self.linear(hc)
+        out = torch.sigmoid(x)
+        return out, hc
+    
+    def parameters(self):
+        return [self.weight_ih, self.weight_hh, self.b_ih, self.b_hh]
+
+hidden_size = 16
+learning_rate = 0.01
+
+model = SimpleClassificationRNN(hidden_size)
+
+'''
+task 3: select appropriate criterion and optimizer
+'''
+# criterion = None
+# optimizer = None
+
+criterion = nn.BCELoss()
+optimizer = optim.Adam(model.parameters(), learning_rate)
+
+def cal_accuracy(preds, true_values):
+    preds = torch.where(preds>0.5, 1, 0)
+    acc = torch.sum(1-torch.abs(preds-true_values)) / preds.shape[0]
+    return acc
+# training ...
+
+epochs = 500
+loss_log = []
+for epoch in range(epochs):
+    optimizer.zero_grad()
+    output, _ = model(train_set[:][:, :-1, np.newaxis])
+    # print(output.shape, train_set[:][:, -1].shape)
+    loss = criterion(output.view(-1), train_set[:][:, -1])
+    acc = cal_accuracy(output.view(-1), train_set[:][:, -1])
+    loss.backward()
+    optimizer.step()
+    if epoch % 10 == 0:
+        print("Epoch {}: loss {} acc {}".format(epoch, loss.item(), acc))
+# performance on test set
+
+output, _ = model(test_set[:][:, :-1, np.newaxis])
+loss = criterion(output.view(-1), test_set[:][:, -1])
+acc = cal_accuracy(output.view(-1), test_set[:][:, -1])
+
+print("Test set: loss {} acc {}".format(loss.item(), acc))
+
+
+
+
+
+'''
+
+## Example Solution
+# define network
+
+class SimpleClassificationRNN(nn.Module):
+    def __init__(self, hidden_size):
+        super(SimpleClassificationRNN, self).__init__()
+        self.rnn = nn.RNN(input_size=1,
+                          hidden_size=hidden_size,
+                          batch_first=True,
+                          num_layers=1)
         self.linear = nn.Linear(hidden_size, 1)
 
     def forward(self, seq, hc=None):
-        tmp, (hc, cc) = self.rnn(seq, hc)
+        tmp, hc = self.rnn(seq, hc)
         out = torch.sigmoid(self.linear(hc[-1, ... ,:]))
-        return out, hc, cc
+        return out, hc
 hidden_size = 16
 learning_rate = 0.01
 
@@ -73,7 +140,7 @@ epochs = 500
 loss_log = []
 for epoch in range(epochs):
     optimizer.zero_grad()
-    output, _, _ = model(train_set[:][:, :-1, np.newaxis])
+    output, _ = model(train_set[:][:, :-1, np.newaxis])
     loss = criterion(output.view(-1), train_set[:][:, -1])
     acc = cal_accuracy(output.view(-1), train_set[:][:, -1])
     loss.backward()
@@ -82,8 +149,10 @@ for epoch in range(epochs):
         print("Epoch {}: loss {} acc {}".format(epoch, loss.item(), acc))
 
 # you can also implement early stopping here ...
-output, _, _ = model(test_set[:][:, :-1, np.newaxis])
+output, _ = model(test_set[:][:, :-1, np.newaxis])
 loss = criterion(output.view(-1), test_set[:][:, -1])
 acc = cal_accuracy(output.view(-1), test_set[:][:, -1])
 
 print("Test set: loss {} acc {}".format(loss.item(), acc))
+
+'''
